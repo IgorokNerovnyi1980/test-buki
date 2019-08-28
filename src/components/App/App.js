0370fe1ba838fe90144723ecf50732ee9a5b51fd
IgorokNerovnyi1980 <i.nerovnyi@gmail.com>
2019-08-28 15:54:30 +0300

    add npm pacages - lintr,gh-pages, prop-types, shortid, axios, redux

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import styles from "./App.module.css";
+
+class App extends Component {
+  render() {
+    return (
+      <header className={styles.header}>
+        <h1>Component App</h1>
+      </header>
+    );
+  }
+}
+
+export default App;
