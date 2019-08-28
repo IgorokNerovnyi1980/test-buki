cec8bec8a355dc4b65c38329a0f0b5ec77624df9
IgorokNerovnyi1980 <i.nerovnyi@gmail.com>
2019-08-28 18:30:19 +0300

    Updates

---
+++ precache-manifest.5d31663fa945d7417e1134269fcf96eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f7404a44d5389a1e9267cfd3d45a87e",
+    "url": "/test-buki/index.html"
+  },
+  {
+    "revision": "40189ecd6df94c386851",
+    "url": "/test-buki/static/css/main.d8c0cd6f.chunk.css"
+  },
+  {
+    "revision": "40cdb3b3312a16f5ba1b",
+    "url": "/test-buki/static/js/2.ed799983.chunk.js"
+  },
+  {
+    "revision": "40189ecd6df94c386851",
+    "url": "/test-buki/static/js/main.dd7775d6.chunk.js"
+  },
+  {
+    "revision": "3313f400b7c8683c7294",
+    "url": "/test-buki/static/js/runtime~main.ad850c60.js"
+  }
+]);
