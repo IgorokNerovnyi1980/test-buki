d438ab2437414f69b94ab3bfae6ff1629c885876
IgorokNerovnyi1980 <i.nerovnyi@gmail.com>
2019-08-29 16:40:52 +0300

    Updates

---
+++ precache-manifest.718572ce9f65ed344a0f4c2b83208ad6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626b51c928a861cdcdc2fde5c6a7903b",
+    "url": "/test-buki/index.html"
+  },
+  {
+    "revision": "14c72155718d3ab53504",
+    "url": "/test-buki/static/css/main.198a5ee1.chunk.css"
+  },
+  {
+    "revision": "20f631e3b6053ab550a1",
+    "url": "/test-buki/static/js/2.502a2f57.chunk.js"
+  },
+  {
+    "revision": "14c72155718d3ab53504",
+    "url": "/test-buki/static/js/main.b02a839f.chunk.js"
+  },
+  {
+    "revision": "3313f400b7c8683c7294",
+    "url": "/test-buki/static/js/runtime~main.ad850c60.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test-buki/precache-manifest.3742ca12eda813d9e56018306c33beac.js"
+  "/test-buki/precache-manifest.718572ce9f65ed344a0f4c2b83208ad6.js"
 );
 
 self.addEventListener('message', (event) => {
